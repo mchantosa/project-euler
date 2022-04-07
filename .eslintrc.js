3995237c44e963c0fcac67875fe1ac7a0ca96483
Oak Chantosa <oak.chantosa@hingehealth.com>
2022-04-07 11:56:28 -0700

    Fix for loop in function

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
