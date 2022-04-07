220021e241eb956cf8f1ac015ec3d5f2c90bdbae
Megan Chantosa <megan@Megans-MacBook-Air.local>
2022-04-07 13:23:33 -0700

    completed pe-1

---
+++ test/pe-1.js
@@ -0,0 +1,6 @@
+const findSumOfMultiples35 = require("../lib/pe-1");
+
+console.log(findSumOfMultiples35(10) === 23);  //true
+console.log(findSumOfMultiples35(5) === 3);  //true
+console.log(findSumOfMultiples35(3) === 0);  //true
+console.log(findSumOfMultiples35(1000));  //233168
